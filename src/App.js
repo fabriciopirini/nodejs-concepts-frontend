3107cd3fcc3574b4903f371a86ad6dbfaedd689c
Fabricio Pirini <fabricio.pirini@gmail.com>
2020-06-19 23:46:29 +0200

    Change text to English

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
           <li key={repo.id}>
             {repo.title}
             <button onClick={() => handleRemoveRepository(repo.id)}>
-              Remover
+              Remove
             </button>
           </li>
         )}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Add</button>
     </>
   );
 }
